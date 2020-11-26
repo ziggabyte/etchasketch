0ea30958d14b992a56259436168cd5a76ab0981d
ziggabyte <ziggi.willpower@gmail.com>
2020-11-26 16:54:01 +0100

    nästan klar
    Börjat med layout, fortfarande bugg kring opaciteten, som fastnar

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
+"use strict";
+
 const sketchContainer = document.getElementById("sketchContainer");
 const allSketchDivs = document.getElementsByClassName("sketchDiv");
 const clearButton = document.getElementById("clearButton");
 const newButton = document.getElementById("newButton");
+const randomColorButton = document.getElementById("randomColorButton");
+const gradualColorButton = document.getElementById("gradualColorButton");
+let colorFunction;
 let divArray = [];
 
-// Gör så rutorna i grid ändrar färg
-function colorChange() {
+// tar bort existerande event listener
+function removeLastMouseoverEvent(colorFunction) {
+    for (let i = 0; i < divArray.length; i++) {
+        divArray[i].removeEventListener("mouseover", colorFunction);
+    }
+}
+
+// lägger till en event listener, tar in en färg-funktion sparad i variabeln colorFunction
+function assignMouseoverEvent(colorFunction) {
+    for (let i = 0; i < divArray.length; i++) {
+        divArray[i].addEventListener("mouseover", colorFunction);
+    }
+}
+
+// Gör så rutorna i grid ändrar till startfärg
+function startColor() {
     this.style.backgroundColor = "peachpuff";
 }
 
+// Ändrar rutans färg till en random
+function randomColor() {
+    let color = (Math.random() * 0xfffff * 1000000).toString(16); // slumpar en hexkod
+    color = "#" + color.slice(0, 6); // kortar den till rätt storlek o gör till string med # 
+    this.style.backgroundColor = color;
+}
+
+// Ändrar rutans färg till 10% svart vid varje hover
+function gradualColor() {
+    this.style.opacity = Number(this.style.opacity) + 0.1;
+    this.style.backgroundColor = "black";
+}
+
 // Ändrar alla rutors färg till vit 
-function clearColor() {
+function clearBoard() {
     for (let i = 0; i < allSketchDivs.length; i++) {
-        allSketchDivs[i].style.backgroundColor = "white";
+        allSketchDivs[i].style.backgroundColor = "#e7e4df";
     }
 }
 
 // Tar bort alla appended children från Sketch container
-function clearDivs() {
+function removeAllDivs() {
     sketchContainer.innerHTML = " ";
 }
 
@@ -36,30 +68,14 @@ function readNumber() {
 
 // skapar nytt board utifrån användarens önskemål om storlek
 function makeNewBoard() {
-    clearColor();
-    clearDivs();
+    clearBoard();
+    removeAllDivs();
     divArray.length = 0;
     let side = readNumber();
-    setDivs(side);  
+    makeDivs(side);
 }
 
-function setDivs(side) {
-    // skapar divar och ger dem klassen sketchDiv
-    for (let i = 0; i < side * side; i++) {
-        divArray.push(document.createElement("div"));
-        divArray[i].setAttribute("class", "sketchDiv");
-        sketchContainer.appendChild(divArray[i]);
-    }
-
-    // Ger alla startrutor event listeners för klick så de ändrar färg
-    for (let i = 0; i < allSketchDivs.length; i++) {
-        allSketchDivs[i].addEventListener("mouseover", colorChange);
-    }
-
-    divSize(side); // Sätter bredd/höjd på div:arna
-}
-
-// tilldelar varje div en storlek i procent
+// ger divar en storlek utifrån antal rutor användaren skrivit in
 function divSize(side) {
     let size = 100 / side;
     let sizeString = size + "%";
@@ -69,11 +85,32 @@ function divSize(side) {
     }
 }
 
+function makeDivs(side) {
+    // skapar divar och ger dem klassen sketchDiv
+    for (let i = 0; i < side * side; i++) {
+        divArray.push(document.createElement("div"));
+        divArray[i].setAttribute("class", "sketchDiv");
+        sketchContainer.appendChild(divArray[i]);
+    }
+
+    assignMouseoverEvent(startColor); // ger dem event listeners så de ändrar färg
+
+    divSize(side); // Sätter bredd/höjd på div:arna
+}
+
 // Startar sidan med ett 4x4 grid
-setDivs(4);
+makeDivs(4);
 
 
 // Kör respektive funktion vid knappklick
-clearButton.addEventListener("click", clearColor);
+clearButton.addEventListener("click", clearBoard);
 newButton.addEventListener("click", makeNewBoard);
+randomColorButton.addEventListener("click", () => { 
+    clearBoard(); 
+    removeLastMouseoverEvent(gradualColor); 
+    assignMouseoverEvent(randomColor); });
+gradualColorButton.addEventListener("click", () => { 
+    clearBoard(); 
+    removeLastMouseoverEvent(randomColor); 
+    assignMouseoverEvent(gradualColor); })
 
